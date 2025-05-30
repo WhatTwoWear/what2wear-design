aaf6da6bd8e9974dd9206e07cd6941adb5293b1b
WhatTwoWear <whattwoware@gmail.com>
2025-05-30 20:27:27 +0200

    git add next.config.mjs git commit -m "Fix: add alias @ for path resolution" git push

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 /** @type {import('next').NextConfig} */
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -9,6 +16,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  }
+};
 
-export default nextConfig
+export default nextConfig;
