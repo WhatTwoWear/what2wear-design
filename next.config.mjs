429ba3ef11f841ffcfc8bb0ead69e83037bf16e4
WhatTwoWear <whattwoware@gmail.com>
2025-05-30 21:17:20 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
-// next.config.mjs
-import path from 'path'
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -15,6 +19,6 @@ const nextConfig = {
     config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
